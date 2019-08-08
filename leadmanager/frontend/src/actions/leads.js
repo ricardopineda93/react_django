c49b3f47d33fcdc1be219d9c448a57ec048e9036
Ricardo Pineda <richay718@gmail.com>
2019-08-07 20:00:27 -0400

    adding confirmation messages to deleting/adding leads

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+import { createMessage } from './messages';
 import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from './types';
 
 // GET LEADS
@@ -20,6 +20,7 @@ export const deleteLead = id => dispatch => {
   axios
     .delete(`api/leads/${id}/`)
     .then(res => {
+      dispatch(createMessage({ deleteLead: 'Lead deleted!' }));
       dispatch({
         type: DELETE_LEAD,
         payload: id
@@ -33,6 +34,7 @@ export const addLead = lead => dispatch => {
   axios
     .post(`api/leads/`, lead)
     .then(res => {
+      dispatch(createMessage({ addLead: 'Lead added!' }));
       dispatch({
         type: ADD_LEAD,
         payload: res.data
--- leadmanager/frontend/src/components/leads/Leads.js
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { getLeads, deleteLead, addLead } from '../../actions/leads';
+import { getLeads, deleteLead } from '../../actions/leads';
 
 export class Leads extends Component {
   static propTypes = {
